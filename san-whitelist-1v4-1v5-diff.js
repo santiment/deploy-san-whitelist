a37c4c722a77674e644cbefda953ac3d8729dc96
Tzanko Matev <tsanko@gmail.com>
2017-07-02 22:24:01 +0300

    Santiment whitelist v1.5

---
+++ san-whitelist-1v4-1v5-diff.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { addr: "0xF46054694B937160f8a1B2C3B4C958E4E46c2F21", min: 235, max: 282 }
+, { addr: "0xa91A30500A60258dC83F08bD7E29086F19E75E10", min: 80, max: 96 }
+, { addr: "0x68007d79f0b374cf2dca33d9ffc35578584ce1f8", min: 0.5, max: 1 }
+, { addr: "0x3A1aA0A96632AD25c5B370aafCE364edA27F6c6f", min: 40, max: 48 }
+, { addr: "0x4a2101ED7873a708158EFC43BF2FCa88630539Cf", min: 1, max: 5 }
+, { addr: "0x57593c260Ef8c20a71aEB52Fc2D62F3f485C3A32", min: 30, max: 36 }
+, { addr: "0xbFa362cF5Ba6265f71b426a6704f30CE52387D8b", min: 0.5, max: 1 }
+];
