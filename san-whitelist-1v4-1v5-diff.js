d2b2dae3f1b2f804e13d114d6e779069709ba7e0
Tzanko Matev <tsanko@gmail.com>
2017-07-03 00:02:44 +0300

    Added missed address

--- san-whitelist-1v4-1v5-diff.js
+++ san-whitelist-1v4-1v5-diff.js
@@ -6,4 +6,5 @@ module.exports = [
 , { addr: "0x4a2101ED7873a708158EFC43BF2FCa88630539Cf", min: 1, max: 5 }
 , { addr: "0x57593c260Ef8c20a71aEB52Fc2D62F3f485C3A32", min: 30, max: 36 }
 , { addr: "0xbFa362cF5Ba6265f71b426a6704f30CE52387D8b", min: 0.5, max: 1 }
+, { addr: "0x0476fCd55967789e5Be3796ACFD734c7721B8F62", min: 30, max: 0 }
 ];
