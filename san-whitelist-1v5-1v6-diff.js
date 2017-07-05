8190c1aace380476bec9f764842214ab00923d3d
Tzanko Matev <tsanko@gmail.com>
2017-07-05 08:12:19 +0300

    Whitelist version 1.6 - post crowdsale start

---
+++ san-whitelist-1v5-1v6-diff.js
@@ -0,0 +1,9 @@
+module.exports = [
+{ addr: "0x3c0b16781c296615d45b8533fb65CD92C5167205", min: 2, max: 5 }
+, { addr: "0xD51e2fa6F61F6F3c97E21Bb91B36AB974B72bf22", min: 3, max: 10 }
+, { addr: "0xFF7BB8e1D6AEE876376677C65278229B48B10Dc0", min: 10, max: 12 }
+, { addr: "0xA5Ba162c959ea6a8FcC93aea45Dc2451EE689A76", min: 250, max: 300 }
+, { addr: "0xcd8862D38E1463cCa235b29368917257a45bCe39", min: 50, max: 60 }
+, { addr: "0x31d4f39A5B392387B8c74c592aE063D7849D985C", min: 2, max: 0 }
+, { addr: "0xb081319e9fd204a60592607596e6580447076c23", min: 2, max: 10 }
+];
